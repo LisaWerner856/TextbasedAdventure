10fb5ce4a58592ea57d1db67e367e4224f0990b5
LAPTOP-MJPMG95J\lisaw <lisawerner17lw@gmail.com>
2021-03-14 10:56:51 +0100

    VS Project setup

---
+++ TextbasedAdventuregame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextbasedAdventuregame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
